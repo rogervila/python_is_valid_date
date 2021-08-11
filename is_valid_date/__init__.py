58f4142e217363fa31c46af38fa53c18f33a4b65
Roger Vilà <rogervila@me.com>
2021-08-11 13:16:59 +0200

    return true for date and datetime instances

--- is_valid_date/__init__.py
+++ is_valid_date/__init__.py
@@ -1,9 +1,13 @@
+from datetime import date, datetime
 from dateutil.parser import parse
 
 
-def check(date) -> bool:
+def check(date_to_check) -> bool:
+    if isinstance(date_to_check, date) or isinstance(date_to_check, datetime):
+        return True
+
     try:
-        parse(date)
+        parse(date_to_check)
         return True
     except:
         return False
--- tests/test_is_valid_date.py
+++ tests/test_is_valid_date.py
@@ -1,11 +1,16 @@
 import unittest
 from is_valid_date import check
+from datetime import date, datetime
 
 
 class test_is_valid_date(unittest.TestCase):
-    def test_dates(self):
+    def test_string_dates(self):
         self.assertTrue(check('1714-09-11'))
         self.assertFalse(check('not a date'))
+
+    def test_date_and_datetime_instances(self):
+        self.assertTrue(check(date.today()))
+        self.assertTrue(check(datetime.now()))
 
 
 if __name__ == '__main__':
