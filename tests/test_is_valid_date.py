8c15eb09644c2b3625e738f8504e722773cb9415
Roger Vilà <rogervila@me.com>
2022-04-04 10:49:48 +0200

    [Feature] Typehinting (#9)

--- tests/test_is_valid_date.py
+++ tests/test_is_valid_date.py
@@ -1,6 +1,6 @@
 import unittest
-from is_valid_date import check
 from datetime import date, datetime
+from is_valid_date import check
 
 
 class test_is_valid_date(unittest.TestCase):
@@ -11,6 +11,15 @@ class test_is_valid_date(unittest.TestCase):
     def test_date_and_datetime_instances(self):
         self.assertTrue(check(date.today()))
         self.assertTrue(check(datetime.now()))
+
+    def test_other_types(self):
+        class Foo:
+            pass
+
+        values = [b'not a date', {}, [], 288, 42.0, Foo, Foo()]
+
+        for value in values:
+            self.assertFalse(check(value))
 
 
 if __name__ == '__main__':
